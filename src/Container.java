bcfbc2258453afedd08bfe0d6dfc75b9d9626585
Davko <66025445+Davko-S@users.noreply.github.com>
2020-11-09 19:47:04 +0100

    Added constructor in Container class

--- src/Container.java
+++ src/Container.java
@@ -4,4 +4,11 @@ public class Container {
     int length;
     int height;
 
+    public Container(String name, int width, int length, int height) {
+        this.name = name;
+        this.width = width;
+        this.length = length;
+        this.height = height;
+    }
+
 }
